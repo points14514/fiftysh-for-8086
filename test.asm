e6489da3b4dd9e4255b6953ec14a9ded34193633
points14514 <3790010758@qq.com>
2025-08-08 19:12:06 +0800

    Update test.asm

--- test.asm
+++ test.asm
@@ -1,12 +1,12 @@
-组织0x7c00
-位16
+org 0x7C00
+bits 16
 
-开始：
-MOV ax，0xB800
-MOV es，ax
-MOV字节[es:0]，'A'
-MOV字节[es:1]，0x1F
-JMP$
+start:
+    mov ax, 0xB800
+    mov es, ax
+    mov byte [es:0], 'A'
+    mov byte [es:1], 0x1F
+    jmp $
 
-乘以510-($-$$)db0
-DW0xAA55
+times 510-($-$$) db 0
+dw 0xAA55
