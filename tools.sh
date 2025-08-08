e65b77f8be31737594eeaca1775c87f6a6dbd386
points14514 <3790010758@qq.com>
2025-08-08 19:15:09 +0800

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,8 +1,8 @@
 #安装NASM(全网汇编程序)
-sudo apt-get安装nasm
+sudo apt-get install nasm
 
 #安装QEMU(含8086模拟)
-sudo apt-get安装qemu-system-x86
+sudo apt-get install qemu-system-x86
 
 #安装16位C编译器(可选)
-sudo apt-get安装openwatcom
+sudo apt-get install openwatcom
